051566de02317afa2a475cf369a4c7d3554c652e
rahul ali <82302787+Rahulfordev@users.noreply.github.com>
2023-10-28 22:06:20 +0600

    Create max-value.js

---
+++ max-value.js
@@ -0,0 +1,8 @@
+//*** Max value fiend
+const arr = [3, 5, 6, 8, 79, 99];
+let maxValue = 0;
+for (let i = 0; i < arr.length; i++) {
+  maxValue = arr[i];
+  maxValue >= arr[i];
+}
+console.log(maxValue);
