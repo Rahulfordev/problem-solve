f31b78c25237f8d0d084cb3d740d89625d11184e
rahul ali <82302787+Rahulfordev@users.noreply.github.com>
2023-10-28 22:02:31 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//**** swap
+function sw(a, b) {
+  a = a + b; //11
+  b = a - b; // 11-6 =5
+  a = a - b; //11-5=6
+  return `a:${a} b:${b}`;
+}
+console.log(sw(5, 6));
+
+//*** another way
+let a = 5;
+let b = 6;
+let temp = a;
+
+a = b;
+b = temp;
+console.log(a, b);
