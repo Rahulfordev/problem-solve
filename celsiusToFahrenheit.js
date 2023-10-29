048c8739710f4f14485498d1f68e176b2436ce0c
rahul ali <82302787+Rahulfordev@users.noreply.github.com>
2023-10-29 23:45:07 +0600

    Create celsiusToFahrenheit.js

---
+++ celsiusToFahrenheit.js
@@ -0,0 +1,11 @@
+function farenhite(enterCalcTemp) {
+  return enterCalcTemp * (9 / 5) + 32;
+}
+
+console.log(farenhite(50));
+
+function calc(enterFarTemp) {
+  return ((enterFarTemp - 32) * 5) / 9;
+}
+
+console.log(calc(100));
