5aed06273205053299702a80033d313e3d264747
rahul ali <82302787+Rahulfordev@users.noreply.github.com>
2023-10-30 10:02:46 +0600

    Update multiplication-table.js

--- multiplication-table.js
+++ multiplication-table.js
@@ -1,9 +1,9 @@
 //** multiplication table
-function multiplication(multipleNumber) {
+function multiplication(multipleNumber, range) {
   let num = 0;
-  for (let i = 0; i <= 10; i++) {
+  for (let i = 1; i <= range; i++) {
     num = multipleNumber * i;
     console.log(`${multipleNumber} * ${i} = ${num}`);
   }
 }
-multiplication(8);
+multiplication(8, 20);
