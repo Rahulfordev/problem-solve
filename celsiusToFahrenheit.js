2af3b39360b44e05089f7f4a9e741ac5c375441f
rahul ali <82302787+Rahulfordev@users.noreply.github.com>
2023-10-30 07:14:06 +0600

    Update celsiusToFahrenheit.js

--- celsiusToFahrenheit.js
+++ celsiusToFahrenheit.js
@@ -2,10 +2,10 @@ function farenhite(enterCalcTemp) {
   return enterCalcTemp * (9 / 5) + 32;
 }
 
-console.log(farenhite(50));
+console.log(farenhite(38) + "°F");
 
 function calc(enterFarTemp) {
-  return ((enterFarTemp - 32) * 5) / 9;
+  return (enterFarTemp - 32) * (5 / 9);
 }
 
-console.log(calc(100));
+console.log(calc(32) + "°C");
